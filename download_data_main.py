a078f6da79b0ce97762e6e8704f5f643f49842d9
kowshikchills <kowshikchilamkurty@gmail.com>
2023-03-06 16:43:28 +0100

    bug in main fixed

--- download_data_main.py
+++ download_data_main.py
@@ -1,7 +1,9 @@
 from src.download_wav_files import Audio_File_Download
+import os
 
 AFD = Audio_File_Download()
-AFD.update_status_file()
+if not os.path.isfile('ids_data/ids_status.csv'):
+    AFD.update_status_file()
 
 def main():
     while True:
