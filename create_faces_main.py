1b81acc9b35791450ab73ffa9668091ce10ac8ec
kowshikchills <kowshikchilamkurhy@gmail.com>
2023-03-25 11:50:35 +0100

    push latest changes

--- create_faces_main.py
+++ create_faces_main.py
@@ -1,9 +1,18 @@
 from src.create_faces_video import CreateFaces
-
+import time
+import glob
 CF = CreateFaces()
 
 def main():
     while True:
-        CF.get_all_faces_from_folder()
+        if len(glob.glob('video_data/*')) > 2:
+            try:
+                CF.get_all_faces_from_folder()
+            except:
+                print('failed')
+                time.sleep(120)
+        else:
+            print('waiting for video files')
+            time.sleep(120)
 
 main()
