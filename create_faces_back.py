1bd24b23664c560b148917b5231cd7c1d175f0b2
kowshikchills <kowshikchilamkurhy@gmail.com>
2023-04-04 14:13:13 +0200

    push latest changes

---
+++ create_faces_back.py
@@ -0,0 +1,18 @@
+from src.create_faces_video import CreateFaces
+import time
+import glob
+CF = CreateFaces(type_='back')
+
+def main():
+    while True:
+        if len(glob.glob('video_data/*')) > 2:
+            try:
+                CF.get_all_faces_from_folder()
+            except:
+                print('failed')
+                time.sleep(120)
+        else:
+            print('waiting for video files')
+            time.sleep(120)
+
+main()
--- src/create_faces_video.py
+++ src/create_faces_video.py
@@ -8,13 +8,19 @@ import face_recognition
 import time
 
 class CreateFaces:
-    def __init__(self,):
-        self.faces_path = 'faces'
+    def __init__(self, type_='front'):
+        self.faces_path = 'faces',
+        self.type_ = type_
 
     def get_video_file(self):
         files = [i for i in glob.glob('video_data/*') if ('.csv' not in i) and ('.mp4' not in i)]
-        id_ = files[0].split('/')[1]
-        return(files[0],id_)
+        if self.type_  == 'front':
+            id_ = files[0].split('/')[1]
+            return(files[0],id_)
+        else:
+            id_ = files[-1].split('/')[1]
+            return(files[-1],id_)
+
 
     def get_faces_file(self, img_loc):
         image = face_recognition.load_image_file(img_loc)
@@ -37,7 +43,8 @@ class CreateFaces:
             if len(self.face_pil_images) != 0:
                 for i in range(len(self.face_pil_images)):
                     pil_image = self.face_pil_images[i]
-                    pil_image.save(self.faces_path + '/'+id_+'_'+frame+ '_face_'+ str(i) +'.png')
+
+                    pil_image.save('faces' + '/'+id_+'_'+frame+ '_face_'+ str(i) +'.png')
         import shutil
         shutil.rmtree(self.folder) 
 
