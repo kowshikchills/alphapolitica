53bbe399c6d21978a36efc4f0bec7a512dbfb714
kowshikchills <kowshikchilamkurhy@gmail.com>
2023-05-14 17:38:57 +0200

    push latest

---
+++ scripts/face_rec_db_search.py
@@ -0,0 +1,11 @@
+import glob
+from src.face_search_utils import *
+import json
+from tqdm import tqdm
+json_dict = {}
+for face_loc in tqdm(glob.glob('../alphapolitica_faces/*')):
+    face_id = face_loc.split('../alphapolitica_faces/')[1]
+    json_dict[face_id] = matching_ids(face_loc)
+
+with open("../data/facerec_results.txt", "w") as fp:
+    json.dump(json_dict, fp)
